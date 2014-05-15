45530a5019600dfc3282360c4d87978a73fcb946
rm68878 <rm68878@L202MICRO14.fiap.com.br>
2014-05-15 11:34:03 -0300

    Criou Carrinho Vazio

--- src/br/calebe/exemplos/ex01/Produto.java
+++ src/br/calebe/exemplos/ex01/Produto.java
@@ -10,6 +10,10 @@ public class Produto {
         this.preco = preco;
     }
 
+    Produto() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public double getPreco() {
         return preco;
     }
